3a7e95d234c09e6df3ae0da40b50459c0196f733
JFcoDiaz <jfcodiaz@gmail.com>
2025-04-10 07:12:36 -0600

    🔧 [#8] chore - initial DevContainer setup with custom DX scripts for Vite 🐳
    - 🧱 Added `.devcontainer/devcontainer.json` to support VSCode Remote Container.
    - 💡 Included helpful VSCode extension recommendations in `.vscode/extensions.json`.
    - 🎨 Customized terminal and editor appearance via `.vscode/settings.json`.
    - 🐋 Updated `docker-compose.yml` and Dockerfile to mount dev helper scripts.
    - 🧪 Created utility scripts: `restart-vite.sh`, `stop-vite.sh`, and `welcome.sh`.
    - 🚀 Enabled custom welcome banner and script integration in container startup.
    - 🔧 Enhanced `vite.config.js` to support custom `HMR_HOST` for hot reloading.
    
    > 🗒️ Temporary file `diff.txt` was added to `.gitignore` to avoid noise in version control.

--- code/vite.config.js
+++ code/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // 👈 importante
+import path from 'path'
+
+const activeHost = process.env.HMR_HOST || 'globalstd-challenge.local'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -13,12 +15,18 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      host: true,
+      allowedHosts: ['globalstd-challenge.local'],
+      host: '0.0.0.0',
       port: 3000,
       strictPort: true,
       watch: {
         usePolling: true,
       },
+      hmr: {
+        host: activeHost,
+        protocol: 'ws',
+        port: 5173
+      }
     },
   }
 })
---
+++ docker/node/scripts/welcome.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+
+cat << "EOF"
+========================================================
+ __                __
+/__ |  _ |_  _  | (_ _|_ _|
+\_| | (_)|_)(_| | __) |_(_|
+       __                    _
+      /   |_  _  |  |  _ __ (_| _
+      \__ | |(_| |  | (/_| |__|(/_ GPLv3
+
+ _              _
+|_) _  |  _    | \ ,  _  _
+|  (_| |<(_)   |_/ | (_| /_ [www.pakodiaz.dev]
+
+=========================================================
+
+👋 Welcome to the local dev container!
+
+[ Into Container ]:
+
+  🔁 To restart Vite:
+     👉  $ restart-vite
+
+  🛑 To stop the dev server:
+     👉  $ stop-vite
+
+  🚀 To start the dev server (attached your terminal):
+     👉  $ start-vite
+
+[ On your loal terminal ]
+
+  📜 To inspect logs:
+     👉  $ docker logs <container-name>
+
+💻 Ejoy your coding!
+
+EOF
